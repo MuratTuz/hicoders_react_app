31a111c26768b902018f6fb6b3d32689b89cc4d3
MuratTuz <murattuzlali@gmail.com>
2021-03-21 16:31:08 +0100

    untracked files are added to git

---
+++ my-react-app/src/data.js
@@ -0,0 +1,22 @@
+const data = [
+    {
+        'topic': 'Input Format',
+        'content': ['First line contains N.N lines follow, each having a PAN number']
+    },
+    {
+        'topic': 'Constraints',
+        'content': [
+            '1 ≤ N ≤ 10',
+            'Each char is an uppercase letter, i.e., char ∈ ["A", "Z"].',
+            'Each digit lies between 0 and 9 i.e., digit ∈ [0,9]',
+            'The lenght of PAN number is always 10, i.e., lenght( PAN )=10',
+            'Every character in PAN is either char or digit satisfying the above constraints.'
+        ]
+    },
+    {
+        'topic': 'Output Format',
+        'content': ['For every PAN number listed, print YES if it is valid and NO if it isn\'t.']
+    }
+]
+
+export default data;
